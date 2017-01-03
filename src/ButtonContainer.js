764f6579ef6094c6521931380517db073c49020e
Michael Lai <mike24567@gmail.com>
2017-01-03 16:22:03 -0500

    took out router functionality

--- src/ButtonContainer.js
+++ src/ButtonContainer.js
@@ -14,15 +14,15 @@ class ButtonContainer extends Component {
         </Button>
       </div>
 
-      <Router history={browserHistory}>
-        <Route path="/" component={App}>
-          <Route path="about" component={About}/>
-          <Route path="users" component={Users}>
-            <Route path="/user/:userId" component={User}/>
-          </Route>
-          <Route path="*" component={NoMatch}/>
-        </Route>
-      </Router>
+      // <Router history={browserHistory}>
+      //   <Route path="/" component={App}>
+      //     <Route path="about" component={About}/>
+      //     <Route path="users" component={Users}>
+      //       <Route path="/user/:userId" component={User}/>
+      //     </Route>
+      //     <Route path="*" component={NoMatch}/>
+      //   </Route>
+      // </Router>
     );
   }
 }
