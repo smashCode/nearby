48247a3a6169536ac57871544388f0e1d1b06ed6
Michael Lai <mike24567@gmail.com>
2017-01-09 16:52:05 -0500

    debug axios api call

--- src/EventListContainer.js
+++ src/EventListContainer.js
@@ -3,7 +3,17 @@ import './index.css';
 import Axios from 'axios';
 
 class EventListContainer extends Component {
-  axios.get('http://api.eventful.com/json/events/search?app_key=VPTzzTTkgzH5nxJZ&where=38.9072,-77.0369&within=5')
+  constructor(props) {
+    super(props)
+    this.state = {
+      distance: 5,
+      lat: 38.9072,
+      long: -77.0369,
+    }
+    console.log("hello" + this.state.distance+this.state.lat)
+  }
+
+  axios.get('http://api.eventful.com/json/events/search?app_key=VPTzzTTkgzH5nxJZ&where='+this.state.lat+','+this.state.long+'&within='+this.state.distance)
   .then(function (response) {
     console.log(response);
   })
