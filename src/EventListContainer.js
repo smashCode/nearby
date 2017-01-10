502b0318445121b811b1a22d8ecf5a4929ee4121
Michael Lai <mike24567@gmail.com>
2017-01-10 18:10:30 -0500

    revise component did mount

--- src/EventListContainer.js
+++ src/EventListContainer.js
@@ -5,31 +5,34 @@ import Axios from 'axios';
 class EventListContainer extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      distance: 5,
-      lat: 38.9072,
-      long: -77.0369,
-    }
-    console.log("hello" + this.state.distance+this.state.lat)
+    this.state = this.state = { items: [] };
   }
 
-makeAPICall(lat, long, distance){
-  return Axios.get(`http://api.eventful.com/json/events/search?app_key=VPTzzTTkgzH5nxJZ&where=${lat},${long}&within=${distance}`)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  componentDidMount() {
+    Axios.get(`http://api.eventful.com/json/events/search?app_key=VPTzzTTkgzH5nxJZ&where=38.9072,-77.0369&within=5`)
+        .then(result=> {
+            this.setState({items:result.json()});
+        });
+  }
 
-}
+// makeAPICall(lat, long, distance){
+//   return Axios.get(`http://api.eventful.com/json/events/search?app_key=VPTzzTTkgzH5nxJZ&where=${lat},${long}&within=${distance}`)
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+//
+// }
 
   render() {
-    this.makeAPICall(38.9072, -77.0369, 5)
+    // this.makeAPICall(38.9072, -77.0369, 5)
     console.log("after render")
     return (
       <div className="container">
-        <h1> hello world</h1>
+        <div>Items:</div>
+          { this.state.items.map(item=> { return <div>{item.name}</div>}) }
       </div>
     );
   }
